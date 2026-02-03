8f43a0a49553942d8eae109f20e4d37bfdba1115
Dmitry Dorogoy <DimonSmart@users.noreply.github.com>
2026-02-03 16:32:06 +0100

    Add time card model and generators

--- lib/features/training/domain/training_item.dart
+++ lib/features/training/domain/training_item.dart
@@ -1,21 +1,29 @@
+import 'time_value.dart';
+
 enum TrainingItemType {
   digits,
   base,
   hundreds,
   thousands,
+  timeExact,
+  timeQuarter,
+  timeHalf,
+  timeRandom,
 }
 
 class TrainingItemId implements Comparable<TrainingItemId> {
   const TrainingItemId({
     required this.type,
-    required this.number,
+    this.number,
+    this.time,
   });
 
   final TrainingItemType type;
   final int? number;
+  final TimeValue? time;
 
   String get storageKey {
-    final suffix = number == null ? '*' : number.toString();
+    final suffix = time?.storageKey ?? number?.toString() ?? '*';
     return '${type.name}:$suffix';
   }
 
@@ -26,16 +34,22 @@ class TrainingItemId implements Comparable<TrainingItemId> {
   bool operator ==(Object other) {
     return other is TrainingItemId &&
         other.type == type &&
-        other.number == number;
+        other.number == number &&
+        other.time == time;
   }
 
   @override
-  int get hashCode => Object.hash(type, number);
+  int get hashCode => Object.hash(type, number, time);
 
   @override
   int compareTo(TrainingItemId other) {
     final typeCompare = type.index.compareTo(other.type.index);
     if (typeCompare != 0) return typeCompare;
+    if (time != null || other.time != null) {
+      final left = time ?? const TimeValue(hour: -1, minute: -1);
+      final right = other.time ?? const TimeValue(hour: -1, minute: -1);
+      return left.compareTo(right);
+    }
     final left = number ?? -1;
     final right = other.number ?? -1;
     return left.compareTo(right);
